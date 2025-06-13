b51d59081e085bb327d4f25913ed53ed20f3ea63
lokesh-repaka <lokeshrepaka4@gmail.com>
2025-06-13 11:15:15 +0530

    project strcuture ready venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Lokesh Repaka",
+    author_email="lokeshrepaka4@gmail.com",
+    packages=find_packages()
+    )
